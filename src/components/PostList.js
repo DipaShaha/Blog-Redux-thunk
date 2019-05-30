3fd62add6cabeea3a8e5af9a28b7a11c209e9b54
DipaShaha <dipashaha92@gmail.com>
2019-05-31 02:45:19 +0600

    First commit

---
+++ src/components/PostList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+class PostList extends Raect.Component{
+	render(){
+		return(
+		<div className="ui container">
+		PostList
+		</div>
+		);
+	}
+}
+export default PostList;
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import {combineReducers} from 'redux';
+demoReducer(){
+
+}
+
+export default combineReducers({
+	replaceMe:()=>10
+});
